f86e917c33ed890cc74e9d5786bfc1e4136985c8
Sukhwinder9914 <ss0333696@gmail.com>
2026-01-30 11:26:07 -0500

    fix divide error handling

--- Lab2_Git_Actions/index.js
+++ Lab2_Git_Actions/index.js
@@ -11,11 +11,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) {
-    console.error("Division by zero is not allowed.");
-  }
-  return a / b;
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
 }
+
 
 console.log(`Addition of 14 and 8 is : ${add(14, 8)}`);
 console.log(`Subtraction of 10 and 7 is : ${subtract(10, 7)}`);
