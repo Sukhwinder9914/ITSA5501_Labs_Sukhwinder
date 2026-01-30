a7dfc852a242992df8a0bc7a0885779c79c88450
Sukhwinder9914 <ss0333696@gmail.com>
2026-01-30 11:20:25 -0500

    added division operation

--- Lab2_Git_Actions/index.js
+++ Lab2_Git_Actions/index.js
@@ -7,12 +7,20 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    console.error("Division by zero is not allowed.");
+  }
+  return a / b;
 }
 
 console.log(`Addition of 14 and 8 is : ${add(14, 8)}`);
 console.log(`Subtraction of 10 and 7 is : ${subtract(10, 7)}`);
 console.log(`Multiplication of 4 and 6 is: ${multiply(4, 6)}`);
+console.log(`Division of 5 and 2 is : ${divide(5, 2)}`);
 
 // Export functions for testing
-module.exports = { add, subtract, multiply };
+module.exports = { add, subtract, multiply, divide };
